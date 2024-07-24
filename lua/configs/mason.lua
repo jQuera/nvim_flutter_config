9505b8aefc2978a0387d21b7aa59d2bb0322379c
jquera <rafael2425@gmail.com>
2024-07-24 12:21:02 -0400

    first commit

---
+++ lua/configs/mason.lua
@@ -0,0 +1,20 @@
+return {
+	ensure_installed = {
+		"lua-language-server",
+		"stylua",
+		"rust-analyzer",
+		"pyright",
+		"clangd",
+		"prettier",
+		"rustfmt",
+		"black",
+		"isort",
+		"typescript-language-server",
+		"svelte-language-server",
+		"codelldb",
+		"debugpy",
+		"zls",
+	},
+
+	max_concurrent_installers = 10,
+}
